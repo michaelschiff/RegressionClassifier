e67af4c5acde3b51401d6fd47352ad0db8b5216d
Michael Schiff <schiff.michael@gmail.com>
2013-02-23 19:32:16 -0800

    removed unecessary files

--- testingHandler.scala
+++
@@ -1,2 +0,0 @@
-//This code will take in a test set and read in the learned weights from disk.
-//It will then evaluate the performance of the learned weights
--- trainingHandler.scala
+++
@@ -1,3 +0,0 @@
-//This code will load the serialized input matrices.  Then it will create and train an instance of the regression classifier. It will then store the learned weights to disk
-
-
