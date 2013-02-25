31642fb09a54c3192932ce6212936584650d126a
Class Account <cs294-ad@icluster14.EECS.Berkeley.EDU>
2013-02-25 13:54:02 -0800

    about to make the change to doing everything with sparse mats

--- regressionTrain.scala
+++ regressionTrain.scala
@@ -12,15 +12,18 @@ trainAndTest.main(Array())
   // Y : a column vector of labels.  ith row is the label for the ith col of X
   // a : the step size
   // t : how much error we are willing to accept
-  class trainer(XList: ListBuffer[FMat], YList: ListBuffer[FMat], a: Float, t: Float) {
+  class trainer(XList: ListBuffer[SMat], YList: ListBuffer[FMat], a: Float, t: Float) {
+    println("checking that num examples matches num labels")
     if ( XList.length != YList.length ) { println("ERROR: num examples does not match num labels") }
     var numWeights = XList(0).nrows
+    println("checking that all example blocks have same nrows")
     for ( X <- XList ) { if ( X.nrows != numWeights ) { println("ERROR: all X blocks do not have same nrows") } }
     val THRESHOLD: Float = t
     var ALPHA: Float = a
     var w: FMat = zeros(numWeights ,1)
    
     def gradients(X: FMat, Y:FMat): FMat = {
+      println("called gradients")
       if ( X.ncols != Y.nrows ) { println("ERROR: block dimensions to not match") }
       val combo = (w.t * X).t
       val diff = combo - Y
@@ -32,32 +35,37 @@ trainAndTest.main(Array())
 
     def error(X: FMat, Y:FMat): FMat = {
       val k: FMat = gradients(X, Y)
-      val e: Float = abs(k)
+      val e: FMat = abs(k)
       return e
     }
     
     def predict(x: FMat): Float = (x*w)(0,0)
     
     //setup for training loop
+    println("zipping examples")
     val examples = XList.zip(YList)
     var iters = 0
     var err: FMat =  0.0f
+    println("calculating initial err")
     for ( (e,l) <- examples ) {
-      err += error(e, l)
+      err += error(full(e), l)
     }
-    var errScore: Float = max(err, 1)
+    var errScore: Float = maxi(err, 1)(0,0)
 
     //training loop
+    println("off to the races")
     while ( errScore > THRESHOLD ) {
       for ( (e,l) <- examples ) {
-        w -= gradients(e, l) * ALPHA
+        w -= gradients(full(e), l) * ALPHA
+        println(iters)
       }
       iters += 1
       err = zeros(err.ncols, 1)
       for ( (e,l)  <- examples ) {
-        err += error(e, l)
+        err += error(full(e), l)
       }
-      errScore = max(err,1)
+      errScore = maxi(err,1)(0,0)
+      println(errScore)
       //if ( iters%20 == 0 ) { ALPHA = ALPHA * 0.9f }
     }
   }
@@ -67,7 +75,7 @@ trainAndTest.main(Array())
       val X:FMat = (1 \ 2 \ 3) on (1 \ 1 \ 1) on (1 \ 1 \ 1)
       val Y:FMat = 1 on 2 on 3
       println("X:\n" + X + "\nY:\n" + Y)
-      val classifier = new trainer(new ListBuffer(X), new ListBuffer(Y), 0.001f, 0.00000000001f)
+      val classifier = new trainer(new ListBuffer() += sparse(X), new ListBuffer() += Y, 0.001f, 0.0001f)
       println("Learned weights:\n" + classifier.w)
       println("Weights should be:\n" + X\\Y)
     }
@@ -76,28 +84,31 @@ trainAndTest.main(Array())
   object trainAndTest {
     def main(args: Array[String]) {
       println("loading data")
-      val xList = new ListBuffer()
+      val xList: ListBuffer[SMat] = new ListBuffer()
       for ( i <- 1 to 975 ) { 
         val block: SMat = load("out/TrimmedSparse"+i+".mat", i+"X")
-        xList +=: block
+        xList += block
       }
-      val lastBlock: SMat = load("out/TrimmedSparseLastX.mat", "LastX")
-      xList +=: lastBlock
+      //val lastBlock: SMat = load("out/TrimmedSparseLastX.mat", "LastX")
+      //xList += lastBlock
 
-      var y = load("out/TrimmedSparseY")
+      var y: IMat = load("out/TrimmedSparseY.mat", "Y")
       y = y.t
-      val yList = new ListBuffer()
+      val yList: ListBuffer[FMat] = new ListBuffer()
       for (i <- 0 to y.ncols by 1000) {
-        if (i+999 < y.ncols ) { yList +=: y(?, i to i+999).t }
-        else { yList +=: y(?, i to y.ncols-1).t }
+        if (i+999 < y.ncols ) { yList += FMat(y(?, i to i+999).t) }
+        //else { yList += FMat(y(?, i to y.ncols-1).t) }
       }
       
 
       println("creating and training classifier")
       val classifier = new trainer(xList, yList, 0.001f, 0.000001f)
-      for ( i <- 0 to x.ncols-1 ) { 
-        println("classifier predicted: " + classifier.predict(full(x(?, i)).t) )
-        println("actually was: " + y(0, i))
+      
+      var testX = full(xList(0))
+      var testY = yList(0)
+      for (i <- 0 to testX.ncols) {
+        println("classifier predicted: " + classifier.predict(testX(?, i).t))
+        println("actually was: " + testY(i,0))
       }
     }
   }
