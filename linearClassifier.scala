117d2d2da42ba21c24acabbad62b9036c9691d71
Michael Schiff <schiff.michael@gmail.com>
2013-02-26 17:53:29 -0800

    linearClassifier now makes the following loop:
    pick randomly 98 blocks of X and Y from the XList and YList respectively
    
    performs one iteration of updates on the remaining blocks of XList and
    YList.
    
    calculates and outputs absolute error for the 98 blocks of hold out data
    
    repeat

--- linearClassifier.scala
+++ linearClassifier.scala
@@ -3,119 +3,99 @@ import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
 import BIDMat.Solvers._
 import BIDMat.Plotting._
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
 import scala.math
-
-//lineTest.main(Array())
+import scala.util.Random
+  
 trainAndTest.main(Array())
 
-  // X : a matrix of examples.  Each column is an example, each row is a feature
-  // Y : a column vector of labels.  ith row is the label for the ith col of X
-  // a : the step size
-  // t : how much error we are willing to accept
-  class trainer(XList: ListBuffer[SMat], YList: ListBuffer[FMat], a: Double, t: Double) {
-    println("checking that num examples matches num labels")
-    if ( XList.length != YList.length ) { println("ERROR: num examples does not match num labels") }
-    var numWeights = XList(0).nrows
-    println("checking that all example blocks have same nrows")
-    for ( X <- XList ) { if ( X.nrows != numWeights ) { println("ERROR: all X blocks do not have same nrows") } }
-    val THRESHOLD: Double = t
-    var ALPHA: Double = a
-    var w: FMat = zeros(numWeights ,1)
-   
-    def gradients(X: SMat, Y:FMat): FMat = {
-      if ( X.ncols != Y.nrows ) { println("ERROR: block dimensions to not match") }
-      //val combo = (w.t * X).t
-      val combo = X Tmult(w, null) //X is sparse w is a COLUMN!!!
-      val diff = combo - Y
-      val twice_diff = diff * 2.0f
-      //var gs = X * twice_diff
-      var gs = X SMult(sparse(twice_diff), null)
-      gs = gs /@ X.ncols
-      // DO RIDGE REGULARIZATION HERE
-      return gs
+object trainAndTest {
+  def main(args: Array[String]) {
+    println("loading data")
+    //build buffer of X blocks
+    val xList: ArrayBuffer[SMat] = new ArrayBuffer()
+    for ( i <- 1 to 975 ) { 
+      val block: SMat = load("CountsOut/CountsStemmedX"+i+".mat", i+"CountsStemmedX")
+      xList += block
     }
+    //build buffer of Y blocks
+    var y: IMat = load("CountsOut/CountsY.mat", "CountsY")
+    y = y.t
+    val yList: ArrayBuffer[FMat] = new ArrayBuffer()
+    for (i <- 0 to y.ncols by 1000) {
+      if (i+999 < y.ncols ) { yList += FMat(y(?, i to i+999).t) }
+    }
+    //initialize classifier
+    println("creating and training classifier")
+    val classifier = new trainer(xList, yList, 0.00000001, 0.001)
+  }
+}
 
-    def error(X: SMat, Y:FMat): FMat = {
-      val k: FMat = gradients(X, Y)
-      val e: FMat = abs(k)
-      return e
+class trainer(XList: ArrayBuffer[SMat], YList: ArrayBuffer[FMat], a: Double) {
+  //basic error checking
+  println("checking that num examples matches num labels")
+  if ( XList.length != YList.length ) { println("ERROR: num examples does not match num labels") }
+  var numWeights = XList(0).nrows
+  println("checking that all example blocks have same nrows")
+  for ( X <- XList ) { if ( X.nrows != numWeights ) { println("ERROR: all X blocks do not have same nrows") } }
+  
+  //step size
+  var ALPHA: Double = a
+  //column vector of weights
+  var w: FMat = zeros(numWeights ,1)
+
+  // variables to store hold out data
+  var XTest: ArrayBuffer[SMat] = new ArrayBuffer[SMat]()
+  var YTest: ArrayBuffer[SMat] = new ArrayBuffer[SMat]()
+  
+  //function to calculate gradients for each weight given a block of X and Y from the training sets
+  def gradients(X: SMat, Y:FMat): FMat = {
+    if ( X.ncols != Y.nrows ) { println("ERROR: block dimensions to not match") }
+    val combo = X Tmult(w, null) //X is sparse w is a COLUMN!!!
+    val diff = combo - Y
+    val twice_diff = diff * 2.0f
+    var gs = X SMult(sparse(twice_diff), null)
+    gs = gs /@ X.ncols
+    // DO RIDGE REGULARIZATION HERE
+    return gs
+  }
+
+  //function to calculate performance given a block of X and Y from the test set
+  def error(X: SMat, Y:FMat): Float = sum(abs(X Tmult(w, null) - Y), 1)(0,0)
+  
+  //training loop
+  var iters = 1
+  while (true) { //currently train forever, we are evaluating performance as we go
+    //partition XList into XTrain XTest and YList into YTrain YTest
+    val rng = new Random()
+    for ( i <- 0 to 97 ) { //randomly select 98 matching blocks of X and Y to use as hold out
+      val blockNum = rng.nextInt(XList.size)
+      val xBlock = XList.remove(blockNum)
+      val yBlock = YList.remove(blockNum)
+      XTest += xBlock
+      YTest += yBlock
     }
-    
-    def predict(x: FMat): Float = (x*w)(0,0)
-    
-    //setup for training loop
-    println("zipping examples")
-    val examples = XList.zip(YList)
-    var iters = 0
-    var err: FMat = zeros(numWeights, 1)
-    println("calculating initial err")
-    for ( (e,l) <- examples ) {
+    //zip training X/Y and test X/Y
+    val trainingExamples = XList.zip(YList)
+    val testingExamples = XTest.zip(YTest)
+    //make an adjustment to the weights for every trainingExample
+    for ( (e,l) <- trainingExamples ) {
+      w -= gradients(e,l) * ALPHA
+    }
+    //calculate absolute error for all testingExamples
+    var err: Float = 0.0f
+    for ( (e,l) <- testingExamples ) {
       err += error(e, l)
     }
-    var errScore: Float = sum(err, 1)(0,0)
-    
-    //training loop
-    println("off to the races")
-    while ( errScore > THRESHOLD ) {
-      err = zeros(numWeights, 1)
-      for ( (e,l) <- examples ) {
-        val gs = gradients(e,l)
-        w -= gs * ALPHA
-        err += abs(gs)
-      }
-      iters += 1
-      errScore = sum(err,1)(0,0)
-      println("===============================================================")
-      println("Trained " + iters + "iterations")
-      println("Sum of gradients: " + errScore)
-      println("largest weight: " + maxi(w, 1)(0,0))
-      println("smallest weight: " + mini(w, 1)(0,0))
-      println("weight 1: " + w(0,0))
-      println("weight 2: " + w(1,0))
-      println("===============================================================")
+    println("Iteration " + iters + ".  Absolute error: " + err)
+
+    //put the current test data back into the main list
+    //this effectively shuffles over multiple iterations while keeping
+    //corresponding blocks of X and Y together
+    for ( (tx,ty) <- XTest.zip(YTest) ) {
+      XList += tx
+      YList += ty
     }
+    iters += 1
   }
-
-  object lineTest {
-    def main(args: Array[String]) {
-      val X:FMat = (1 \ 2 \ 3) on (1 \ 1 \ 1) on (1 \ 1 \ 1)
-      val Y:FMat = 1 on 2 on 3
-      println("X:\n" + X + "\nY:\n" + Y)
-      val classifier = new trainer(new ListBuffer() += sparse(X), new ListBuffer() += Y, 0.001, 0.0001)
-      println("Learned weights:\n" + classifier.w)
-      println("Weights should be:\n" + X\\Y)
-    }
-  }
-
-  object trainAndTest {
-    def main(args: Array[String]) {
-      println("loading data")
-      val xList: ListBuffer[SMat] = new ListBuffer()
-      for ( i <- 1 to 975 ) { 
-        val block: SMat = load("CountsOut/CountsStemmedX"+i+".mat", i+"CountsStemmedX")
-        xList += block
-      }
-      //val lastBlock: SMat = load("CountsOut/CountsStemmedLastX.mat", "LastCountsStemmedX")
-      //xList += lastBlock
-
-      var y: IMat = load("CountsOut/CountsY.mat", "CountsY")
-      y = y.t
-      val yList: ListBuffer[FMat] = new ListBuffer()
-      for (i <- 0 to y.ncols by 1000) {
-        if (i+999 < y.ncols ) { yList += FMat(y(?, i to i+999).t) }
-        //else { yList += FMat(y(?, i to y.ncols-1).t) }
-      }
-      
-
-      println("creating and training classifier")
-      val classifier = new trainer(xList, yList, 0.00000001, 0.001)
-      
-      var testX = full(xList(0))
-      var testY = yList(0)
-      for (i <- 0 to testX.ncols-1) {
-        println("classifier predicted: " + classifier.predict(testX(?, i).t))
-        println("actually was: " + testY(i,0))
-      }
-    }
-  }
+}
